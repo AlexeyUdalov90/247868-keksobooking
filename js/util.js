2f2d297b88c34962d1a545ab9616585b4919431e
AlexeyUdalov90 <armiman69@gmail.com>
2017-12-11 21:08:40 +0300

    Создал модуль util.js поместил туда функции общего назначения

---
+++ js/util.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(function () {
+  window.util = {
+    getRandomItem: function (array) {
+      return array[Math.floor(Math.random() * array.length)];
+    },
+
+    getNumberFromRange: function (min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+
+    getRandomListItems: function (array) {
+      var copyArray = array.slice();
+      copyArray.sort(function () {
+        return Math.random() - 0.5;
+      });
+      copyArray.length = Math.floor(Math.random() * copyArray.length);
+      return copyArray;
+    },
+
+    disableItems: function (array, bool) {
+      for (var i = 0; i < array.length; i++) {
+        array[i].disabled = bool;
+      }
+    },
+
+    drawColorBorder: function (item, color) {
+      item.style.borderColor = color;
+    }
+  };
+})();
